f1756288f85ca44f315bd9c579ff927609c6308e
Seth Carney <seth.carney@eloqua.com>
2013-05-16 20:33:12 -0400

    add fix for _=_ that facebook appends to the url

--- assets/js/core.js
+++ assets/js/core.js
@@ -2,6 +2,14 @@ window.Sportsherd = window.Sportsherd || {};
 window.Sportsherd.statechart = Stativus.createStatechart();
 
 $(document).ready(function() {
+  // Change in session redirect behavior. Need to remove #_=_.
+  // https://developers.facebook.com/blog/post/552/
+  // https://github.com/jaredhanson/passport-facebook/issues/12#issuecomment-5913711
+  if (window.location.hash === '#_=_') {
+    window.location.hash = '';
+    history.pushState('', document.title, window.location.pathname);
+  }
+
   $.ajaxSetup({ headers: {'X-CSRF-Token': window.CSRF} });
 
   window.Sportsherd.router = new window.Sportsherd.Router();
