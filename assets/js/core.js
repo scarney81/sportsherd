51af3b2003cd157ac4116120728139cee1e66700
Seth Carney <seth.carney@eloqua.com>
2013-05-16 20:39:43 -0400

    improve facebook fix

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,15 +1,27 @@
 window.Sportsherd = window.Sportsherd || {};
 window.Sportsherd.statechart = Stativus.createStatechart();
 
-$(document).ready(function() {
-  // Change in session redirect behavior. Need to remove #_=_.
-  // https://developers.facebook.com/blog/post/552/
-  // https://github.com/jaredhanson/passport-facebook/issues/12#issuecomment-5913711
+// Change in session redirect behavior. Need to remove #_=_.
+// https://developers.facebook.com/blog/post/552/
+// https://github.com/jaredhanson/passport-facebook/issues/12#issuecomment-5913711
+(function() {
   if (window.location.hash === '#_=_') {
-    window.location.hash = '';
-    history.pushState('', document.title, window.location.pathname);
+    if (window.history && window.history.pushState) {
+      history.pushState('', document.title, window.location.pathname);
+    } else {
+      window.location.hash = '';
+      var position = {
+        top: document.body.scrollTop,
+        left: document.body.scrollLeft
+      };
+      // Restore the scroll offset, should be flicker free
+      document.body.scrollTop = position.top;
+      document.body.scrollLeft = position.left;
+    }
   }
+})();
 
+$(document).ready(function() {
   $.ajaxSetup({ headers: {'X-CSRF-Token': window.CSRF} });
 
   window.Sportsherd.router = new window.Sportsherd.Router();
