d894b17b00ca51833d9223da0d333476bfdcb6d2
Seth Carney <seth.carney@eloqua.com>
2013-05-29 16:06:48 -0400

    changes

--- facebook/index.js
+++ facebook/index.js
@@ -2,16 +2,29 @@
 var _ = require('underscore');
 var request = require('request');
 
-var baseUrl = 'https://graph.facebook.com/';
+var facebook = function(options) {
+  options = options || {};
 
-var facebook = function(accessToken) {
-  this._accessToken = accessToken;
+  this._accessToken = options.accessToken;
+  this._baseUrl = options.baseUrl || 'https://graph.facebook.com/';
+};
+
+facebook.prototype._buildHeaders = function() {
+  return { Authorization: 'OAuth '+this._accessToken };
+};
+
+facebook.prototype._buildRequestOptions = function(uri) {
+  var headers = this._buildHeaders();
+  return { uri: uri, headers: headers, json: true };
+};
+
+facebook.prototype._execute = function(options) {
+
 };
 
 facebook.prototype.getGroups = function(userId, callback) {
   var uri = baseUrl+userId+'/groups?fields=privacy';
-  var headers = { Authorization: 'OAuth '+this._accessToken};
-  var options = { uri: uri, headers: headers, method: 'GET', json: true };
+  var options = this._buildRequestOptions(uri);
   
   request(options, function(err, response, body) {
     if (err) return callback(err);
